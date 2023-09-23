2a3dc4e4155533402595f9d5b54543a688666867
grekuu <gracjanprusik@gmail.com>
2023-09-23 15:36:42 +0200

    feat(InfoTable): Sorting by time

--- src/components/InfoTable/InfoTable.tsx
+++ src/components/InfoTable/InfoTable.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './infoTable.scss'
 import { Container } from 'react-bootstrap'
 import Table from 'react-bootstrap/Table'
@@ -6,6 +7,23 @@ import { selectData } from '../../redux/formSlice'
 
 const InfoTable = () => {
     const formData = useAppSelector(selectData)
+    const [sortedData, setSortedData] = useState([...formData])
+    const [isAscending, setIsAscending] = useState(true)
+    useEffect(() => {
+        const sorted = [...formData].sort((a, b) => {
+            if (isAscending) {
+                return a.time.localeCompare(b.time)
+            } else {
+                return b.time.localeCompare(a.time)
+            }
+        })
+
+        setSortedData(sorted)
+    }, [formData, isAscending])
+
+    const sortByTime = () => {
+        setIsAscending(!isAscending)
+    }
 
     return (
         <Container className="table-container">
@@ -14,12 +32,14 @@ const InfoTable = () => {
                     <tr>
                         <th>#</th>
                         <th>Description</th>
-                        <th>Time</th>
+                        <th onClick={sortByTime} style={{ cursor: 'pointer' }}>
+                            Time {isAscending ? '↑' : '↓'}
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {formData.map((item, id) => (
-                        <tr>
+                    {sortedData.map((item, id) => (
+                        <tr key={id}>
                             <td>{id + 1}</td>
                             <td>{item.description}</td>
                             <td>{item.time}</td>
