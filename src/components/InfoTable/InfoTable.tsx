412044ba4246b80e1eac9d1693eac6e0723cdf04
grekuu <gracjanprusik@gmail.com>
2023-09-23 15:42:52 +0200

    feat(InfoTable): Table filtering finished

--- src/components/InfoTable/InfoTable.tsx
+++ src/components/InfoTable/InfoTable.tsx
@@ -4,11 +4,14 @@ import { Container } from 'react-bootstrap'
 import Table from 'react-bootstrap/Table'
 import { useAppSelector } from '../../redux/hooks'
 import { selectData } from '../../redux/formSlice'
+import Form from 'react-bootstrap/Form'
 
 const InfoTable = () => {
     const formData = useAppSelector(selectData)
     const [sortedData, setSortedData] = useState([...formData])
     const [isAscending, setIsAscending] = useState(true)
+    const [filterText, setFilterText] = useState('')
+
     useEffect(() => {
         const sorted = [...formData].sort((a, b) => {
             if (isAscending) {
@@ -25,8 +28,23 @@ const InfoTable = () => {
         setIsAscending(!isAscending)
     }
 
+    useEffect(() => {
+        const filteredData = formData.filter((item) =>
+            item.description.toLowerCase().includes(filterText.toLowerCase())
+        )
+        setSortedData(filteredData)
+    }, [filterText, formData])
+
     return (
         <Container className="table-container">
+            <Form.Label>Filter by description</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="FormInputs component"
+                className="filter-input"
+                value={filterText}
+                onChange={(e) => setFilterText(e.target.value)}
+            />
             <Table striped bordered hover>
                 <thead>
                     <tr>
